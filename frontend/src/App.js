37c3bf0598549ed8730dec3ee9a72c5c384a9cdf
Chitrarth <chitrarthrai10@gmail.com>
2024-05-17 12:12:12 +0530

    NewCollections,NewsLetter,Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import ShopCategory from './Pages/ShopCategory';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Shop from './Pages/Shop';
+import Footer from './Components/Footer/Footer';
 
 
 export default function App() {
@@ -24,6 +25,7 @@ export default function App() {
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/login' element={<LoginSignup/>}/>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
