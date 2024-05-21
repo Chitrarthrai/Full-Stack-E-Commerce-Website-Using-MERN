1564decbfb5ec14949e42d2aa9993b065160463e
Chitrarth <chitrarthrai10@gmail.com>
2024-05-21 17:02:50 +0530

    Product, Breadcrum, ProductDisplay used for selecting product and expanding them with details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ export default function App() {
           <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
           <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
           <Route path="/product" element={<Product/>}>
-            <Route path=':productID' element={<Product/>}/>
+            <Route path=':productId' element={<Product/>}/>
           </Route>
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/login' element={<LoginSignup/>}/>
