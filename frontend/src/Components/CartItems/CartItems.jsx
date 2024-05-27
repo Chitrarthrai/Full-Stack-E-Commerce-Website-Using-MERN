36e7ef35d611c6f9548cbe4f331b1ceccc7d450b
Chitrarth <chitrarthrai10@gmail.com>
2024-05-27 17:30:02 +0530

    cartitem jsx and css file complete with designing

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -4,7 +4,7 @@ import { ShopContext } from '../../Context/ShopContext'
 import remove_icon from '../Assets/cart_cross_icon.png'
 
 const CartItems = () => {
-    const {all_product,cartItems,addTOCart,removeFromCart} = useContext(ShopContext)
+    const {all_product,cartItems,removeFromCart} = useContext(ShopContext)
   return (
     <div className='cartitems'>
         <div className="cartitems-format-main">
@@ -20,18 +20,48 @@ const CartItems = () => {
             if(cartItems[e.id]>0)
             {
                 return <div>
-                            <div className="cartitems-format">
-                                <img src={e.image} alt="carticon-product-icon" />
+                            <div className="cartitems-format cartitems-format-main ">
+                                <img src={e.image} alt="" className='carticon-product-icon' />
                                 <p>{e.name}</p>
                                 <p>${e.new_price}</p>
                                 <button className='cartitems-quantity'>{cartItems[e.id]}</button>
                                 <p>${e.new_price*cartItems[e.id]}</p>
-                                <img src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
+                                <img className='carticon-remove-icon' src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
                             </div>
                             <hr />
                         </div>
             }
+            return null;    
         })}
+        <div className="cartitems-down">
+            <div className="cartitems-total">
+                <h1>Cart Totals</h1>
+                <div>
+                    <div className="cartitems-total-item">
+                        <p>Subtotal</p>
+                        <p>${0}</p>
+                    </div>
+                    <hr />
+                    <div className="cartitems-total-item">
+                        <p>Shipping Fee</p>
+                        <p>Free</p>
+                    </div>
+                    <hr />
+                    <div className="cartitems-total-item">
+                        <h3>Total</h3>
+                        <h3>${0}</h3>
+                    </div>
+                </div>
+                <button>PROCEED TO CHECKOUT</button>
+            </div>
+            <div className="cartitems-promocode">
+                <p>f you have a Promo Code, Entre it here</p>
+                <div className="cartitems-promobox">
+                    <input type="text" placeholder='Promo Code'/>
+                    <button>Submit</button>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
