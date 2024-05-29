ff82b864315b4b45baaa15b94dcb42512b6287ae
Chitrarth <chitrarthrai10@gmail.com>
2024-05-29 19:21:31 +0530

    Cart Total in ShopContext and Cartitems

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -25,7 +25,21 @@ const ShopContextProvider = (props) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
     }
 
-    const contextValue = {all_product,cartItems,addToCart,removeFromCart};
+    const getTotalCartAmount = () => {
+        let totalAmount=0;
+        for(const item in cartItems)
+        {
+            if(cartItems[item]>0)
+            {
+                let itemInfo = all_product.find((product)=>product.id===Number(item));
+                totalAmount = totalAmount + itemInfo.new_price * cartItems[item];
+            }
+            return totalAmount;
+            
+        }
+    }
+    
+    const contextValue = {all_product,cartItems,addToCart,removeFromCart,getTotalCartAmount};
 
     return(
         <ShopContext.Provider value={contextValue}>
