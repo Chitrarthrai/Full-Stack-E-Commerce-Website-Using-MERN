f36d3f1171cd4352a465652eab7c81f15477a694
Chitrarth <chitrarthrai10@gmail.com>
2024-06-16 01:39:56 +0530

    made cart page responsive and started backend, added mongodb database, created image storage engine

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+const port=4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+//Database Connection with MongoDB
+mongoose.connect("mongodb+srv://chitrarthrai10:7357084507@cluster0.dl4vpfr.mongodb.net/e-commerce");
+
+//API Creation
+app.get("/",(req,res)=>{
+    res.send("Express App is Running")
+})
+
+//Image Storage Engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage:storage})
+
+//Creating Upload Endpoint for images
+app.use('/images',express.static('upload/images'))
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+app.listen(port,(error)=>{
+    if(!error){
+        console.log("Server Running on Port"+port);
+    }
+    else{
+        console.log("Error: "+error);
+    }
+});
